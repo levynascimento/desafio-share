74d1057d6d2a056fe24adf2c3eea556659c6263e
Levy Martins Nascimento <levymnascimento@hotmail.com>
2023-01-10 20:25:56 -0300

    Frontend do desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import CreateUser from "pages/crud/CreateUser";
+import EditUser from "pages/crud/EditUser";
+import ListUsers from "pages/crud/ListUsers";
+import ShowUser from "pages/crud/ShowUser"
+
+import ListRandomUsers from "pages/randomUsers/ListRandomUsers";
 
 import { Refine } from "@pankod/refine-core";
 import {
-  notificationProvider,
   Layout,
+  notificationProvider,
   ReadyPage,
   ErrorComponent,
 } from "@pankod/refine-antd";
@@ -12,17 +17,44 @@ import "@pankod/refine-antd/dist/reset.css";
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
 import { ColorModeContextProvider } from "contexts";
+import randomCat from "pages/httpCat/randomCat";
+import randomDog from "pages/randomDog/randomDog";
+import { Login } from "pages/login/index";
+
 
 function App() {
+  
   return (
     <ColorModeContextProvider>
       <Refine
-        dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
         notificationProvider={notificationProvider}
         Layout={Layout}
         ReadyPage={ReadyPage}
         catchAll={<ErrorComponent />}
         routerProvider={routerProvider}
+        LoginPage={Login}
+        dataProvider={dataProvider("http://localhost:3333")}
+        resources={[
+          {
+            name: "randomUsers",
+            list: ListRandomUsers,
+          },
+          {
+            name: "httpCat",
+            list: randomCat,
+          },
+          {
+            name: "randomDog",
+            list: randomDog,
+          },
+          {
+            name: "clients",
+            list: ListUsers,
+            create: CreateUser,
+            edit: EditUser,
+            show: ShowUser
+          },
+        ]}
       />
     </ColorModeContextProvider>
   );
